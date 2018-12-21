f5aa38ff7c3ba4f53cb33cf6fbf39a645729d48a
pduchesne <pduchesne@gmail.com>
2018-12-21 13:14:15 +0100

    first commit

---
+++ src/spotify-api-111.test.ts
@@ -0,0 +1,6 @@
+import * as SpotifyWebApi from 'spotify-web-api-js-111';
+
+test('test instantiation', () => {
+    let api: SpotifyWebApi.SpotifyWebApiJs = new SpotifyWebApi();
+    expect(api).not.toBe(undefined);
+});
